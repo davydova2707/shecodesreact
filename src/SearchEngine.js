37b93a97ae51219e633a8d502065f04fc1a18f84
davydova2707 <109292422+davydova2707@users.noreply.github.com>
2022-10-30 15:31:08 +0000

    Weather

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
+import Weather from "./Weather";
 
 export default function SearchEngine() {
-    return (
-        <div>
-          qwe
-        </div>
-      );
+  let [city, setCity] = useState("");
+  let [textCity, setTextCity] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setTextCity(<Weather city={city} />);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="search" placeholder="Type a city" onChange={updateCity} />
+        <input type="submit" value="Search" />
+      </form>
+      {textCity}
+    </div>
+  );
 }
---
+++ src/Weather.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+export default function Weather(props) {
+  let [temperature, setTemperature] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [speed, setSpeed] = useState(null);
+  let [icon, setIcon] = useState(null);
+
+  function getData(response) {
+    setTemperature(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setSpeed(response.data.wind.speed);
+    setIcon(
+      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    );
+  }
+
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=cb83cd89bebae00ada77eb1feb5129d6&units=metric`;
+  axios.get(url).then(getData);
+
+  return (
+    <div>
+      <p></p>
+      <p>City : {props.city}</p>
+      <p>Temperature : {Math.round(temperature)} °C</p>
+      <p>Description : {description}</p>
+      <p>Humidity : {humidity}%</p>
+      <p>Wind : {speed} km/h</p>
+      <p>
+        <img src={icon} />
+      </p>
+    </div>
+  );
+}
